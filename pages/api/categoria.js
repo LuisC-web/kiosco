d8054a8d3f35065e7bc6631e11bcbaa30d1dacfe
luisC <camargoluis19112018@gmail.com>
2022-08-21 20:16:37 -0500

    configurando bases de datos

---
+++ pages/api/categoria.js
@@ -0,0 +1,14 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+
+import { PrismaClient } from '@prisma/client';
+
+const handler = async (req, res) => {
+  try {
+    const prisma = new PrismaClient();
+    const categorias = await prisma.categoria.findMany();
+    res.status(200).json(categorias);
+  } catch (error) {
+    res.status(400).json(error);
+  }
+};
+export default handler;
